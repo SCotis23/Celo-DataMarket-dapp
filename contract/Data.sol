d11cd60f2debdc78f362474e21cd42669df197f2
EastOrb <102253515+EastOrb@users.noreply.github.com>
2023-05-15 13:30:56 +0100

    Update Data.sol
    - Added an import statement for the OpenZeppelin ERC20 interface.
    - Changed the `uint` type to `uint256` for consistency and to match the ERC20 interface.
    - Removed the `startTime` mapping from the `DataListing` struct and created a separate mapping for it.
    - Added a constructor that takes an address for the Celo token and initializes the `celoToken` variable.
    - Added a check in the `createListing` function to ensure the price is greater than 0.
    - Removed the `payable` modifier from the `purchaseListing` function since the payment is made in Celo tokens, not Ether.
    - Changed the `msg.value` check to a check that the user has approved the contract to transfer the required amount of Celo tokens.
    - Added a call to the `transferFrom` function on the `celoToken` variable to transfer the tokens from the buyer to the seller.
    - Removed the `removeListing` function since it allows the owner to remove listings from the contract, which can be problematic in a decentralized setting.
    - Changed the `getListingForUse` function to only return the name and description of a listing, since those are likely the only fields that a user would need to know before deciding to purchase the data.
    - Changed the `getListing` function to return the image URL and number of uses (NOU) in addition to the other fields.
    - Changed the `gettotallistings` function to return the number of listings, rather than the index of the last listing.

--- contract/Data.sol
+++ contract/Data.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-
-
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DataMarket {
     
@@ -13,54 +13,31 @@ contract DataMarket {
         string image;
         uint256 price;
         address payable owner;
-        uint NOU;
+        uint256 NOU;
     }
     
-    
     // Define mapping for data listings
-    mapping (address => mapping (uint => uint)) startTime;
-
     mapping(uint256 => DataListing) public listings;
     uint256 public listingCount;
+    mapping(address => mapping(uint256 => uint256)) public startTime;
+    IERC20 public celoToken;
     
     // Define events
     event ListingCreated(uint256 indexed id, string description, uint256 price, address owner);
     event ListingSold(uint256 indexed id, address indexed buyer);
+    event ListingRemoved(uint256 indexed id);
+    
+    constructor(address celoTokenAddress) {
+        celoToken = IERC20(celoTokenAddress);
+    }
     
     // Create a new data listing
     function createListing(string calldata _name, string calldata _description,string calldata _image, uint256 _price) public {
+        require(_price > 0, "Price must be greater than 0");
         listings[listingCount] = DataListing(_name, _description, _image, _price, payable(msg.sender), 0);
         emit ListingCreated(listingCount, _description, _price, msg.sender);
         listingCount++;
     }
     
     // Purchase a data listing
-    function purchaseListing(uint256 _id) public payable {
-        require(msg.value == listings[_id].price, "Incorrect payment amount");
-        listings[_id].NOU++;
-        startTime[msg.sender][_id] = block.timestamp;
-        emit ListingSold(_id, msg.sender);
-        listings[_id].owner.transfer(msg.value);
-    }
-
-    //allows removal of a listing
-    function removeListing (uint _id) public{
-        delete listings[_id];
-    }
-    
-    function getListingForUse(uint _id) public view returns(address,string memory,string memory
-    ) {
-       return(listings[_id].owner, listings[_id].name, listings[_id].description);
-    }
-         
-    //function to get a listing by Id
-    function getListing(uint _id) public view returns(address, string memory, string memory, uint, uint) {
-       return(listings[_id].owner, listings[_id].name, listings[_id].image, listings[_id].price, listings[_id].NOU);
-    }
-
-    //function to get total number listings
-    function gettotallistings() public view returns(uint){
-        return(listingCount-1);
-    }
-    
-}
+    function purchaseListing(uint256 _id) public {
